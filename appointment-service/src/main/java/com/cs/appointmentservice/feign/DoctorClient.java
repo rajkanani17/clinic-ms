d75c9d4ca0681e77b693094337a02b8a6ac39a04
Rohan Sonawane <rohansonawane1818@gmail.com>
2025-04-14 17:41:20 +0530

    Dockerized all the microservices

--- appointment-service/src/main/java/com/cs/appointmentservice/feign/DoctorClient.java
+++ appointment-service/src/main/java/com/cs/appointmentservice/feign/DoctorClient.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "doctor-service")
+@FeignClient(name = "doctor-service",
+        url="${doctor-service.url}")
 public interface DoctorClient {
     @GetMapping("/api/doctors/{id}")
     DoctorDTO getDoctorById(@PathVariable("id") Long id);
--- appointment-service/src/main/java/com/cs/appointmentservice/feign/PatientClient.java
+++ appointment-service/src/main/java/com/cs/appointmentservice/feign/PatientClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "patient-service")
+@FeignClient(name = "patient-service", url="${patient-service.url}")
 public interface PatientClient {
     @GetMapping("/api/patients/{id}")
     PatientDTO getPatientById(@PathVariable("id") Long id);
